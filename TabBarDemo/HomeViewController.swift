8e2304678f4c4ab6c8eb53bc04ea9026962a9b2e
Patrick M. Roskam <pat.roskam@gmail.com>
2018-04-12 21:03:28 -0700

    tested

---
+++ TabBarDemo/HomeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  HomeViewController.swift
+//  TabBarDemo
+//
+//  Created by Patrick Roskam on 4/12/18.
+//  Copyright © 2018 PatrickMRoskam. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func jumpToCalendarTab(_ sender: UIButton) {
+        tabBarController?.selectedIndex = 1
+    }
+}
